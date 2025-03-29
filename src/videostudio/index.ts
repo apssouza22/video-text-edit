e576fda72eed8743cf894dca0537ca919ec965cf
alexsandro.souza <alexsandro.souza@workday.com>
2025-03-29 13:26:59 +0100

    Add dist folder

--- src/videostudio/index.ts
+++
@@ -1,8 +0,0 @@
-import { setupControls } from "./controls";
-import { setupTimeline } from "./timeline";
-
-import { composition } from "./VideoStudioUtils";
-
-// connect to ui
-setupControls(composition);
-setupTimeline(composition);
