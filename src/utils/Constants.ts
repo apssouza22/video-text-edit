18bc8f3db668acf46d691c8c56b4dce9af406faf
alexsandro.souza <alexsandro.souza@workday.com>
2025-03-29 18:28:18 +0100

    Remove useVideoEditor.ts

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -26,7 +26,7 @@ export default {
     DEFAULT_AUDIO_URL: `https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/${
         isMobileOrTablet ? "jfk" : "ted_60_16k"
     }.wav`,
-    DEFAULT_MODEL: "Xenova/whisper-small",
+    DEFAULT_MODEL: "Xenova/whisper-base",
     DEFAULT_SUBTASK: "transcribe",
     DEFAULT_LANGUAGE: "english",
     DEFAULT_QUANTIZED: true,
